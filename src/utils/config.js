289a848478d7b3d39b08c5f8a1ea4b832a54ea21
nearrainbow <near_rainbow@hotmail.com>
2023-12-07 08:35:06 +0700

    fix bug

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const devConfig = {
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://47.236.25.122",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://47.236.25.128",
 };
 
 const prodConfig = {
