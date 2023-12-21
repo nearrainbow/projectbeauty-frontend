14361b3bda636a402839f508b8e2a26246945420
nearrainbow <near_rainbow@hotmail.com>
2023-12-21 15:02:58 +0700

    update frontend

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,11 +1,16 @@
 import "./CategoryCard.css";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-const CategoryCard = ({ name, image }) => {
+const CategoryCard = ({ name, image}) => {
 
+  const history = useHistory()
+
+  const onSelect = () => {
+    history.push(`/category/${name}`)
+  }
   return (
     <div className="category_cart">
-      <img src={image} alt={name} className="img"/>
+      <img src={image} alt={name} className="img" onClick={onSelect}/>
       <div className="product__info">
         <p className="info__name">{name}</p>
       </div>
